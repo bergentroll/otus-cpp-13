5262432f2b08a70c3d836f4ec885adc38c714885
Anton Karmanov <bergertroll@insiberia.net>
2020-08-02 17:51:19 +0500

    Split Handler implementation

--- inc/handler.hpp
+++ inc/handler.hpp
@@ -1,114 +1,21 @@
 #pragma once
 
-#include <algorithm>
-#include <boost/algorithm/string.hpp>
-#include <boost/lexical_cast.hpp>
 #include <string>
-#include <sstream>
-#include <iomanip>
 
 #include "solver.hpp"
 
 namespace otus {
-  inline std::string formatJoinResult (Solver::JoinType &input) {
-    std::sort(input.begin(), input.end(), [](auto const &a, auto const &b) {
-          return std::get<0>(a) < std::get<0>(b);
-        });
-
-    std::stringstream result { };
-
-    for (auto line: input) {
-      auto id { std::to_string(std::get<0>(line)) };
-      auto const &nameA { std::get<1>(line) };
-      auto const &nameB { std::get<2>(line) };
-
-      result << id << ',' << nameA << ',' << nameB << '\n';
-    }
-
-    result << "OK\n";
-
-    return result.str();
-  }
+  std::string formatJoinResult (Solver::JoinType &input);
 
   // TODO test
   class Handler {
   public:
-    std::string handleCommand(std::string_view command) {
-      std::vector<std::string> tokens;
-      boost::split(tokens, command, [](char c) { return c == ' '; });
-
-      std::transform(
-          tokens[0].begin(),
-          tokens[0].end(),
-          tokens[0].begin(),
-          ::toupper);
-
-      if (tokens[0] == "INSERT") {
-        if (tokens.size() != 4)
-          return "ERR command \"INSERT\" takes exactly three args\n";
-
-        int id;
-        try {
-          id = boost::lexical_cast<int>(tokens[2]);
-        }
-        catch (boost::bad_lexical_cast const &e) {
-          return "ERR invalid id \"" + tokens[2] + "\"\n";
-        }
-
-        auto const &name { tokens[3] };
-
-        if (tokens[1] == "A") {
-          solver.insertToA(id, name);
-          return "OK\n";
-        }
-        else if (tokens[1] == "B") {
-          solver.insertToB(id, name);
-          return "OK\n";
-        }
-        else return "Unknown table \"" + tokens[1] + "\"\n";;
-      }
-      else if (tokens[0] == "TRUNCATE") {
-        if (tokens.size() != 2)
-          return "ERR command \"TRUNCATE\" takes exactly one arg\n";
-        else if (tokens[1] == "A") {
-          solver.truncateA();
-          return "OK\n";
-        }
-        else if (tokens[1] == "B") {
-          solver.truncateB();
-          return "OK\n";
-        }
-        else return "Unknown table \"" + tokens[1] + "\"\n";;
-      }
-      else if (tokens[0] == "INTERSECTION") {
-        return handleIntersection(tokens);
-      }
-      else if (tokens[0] == "SYMMETRIC_DIFFERENCE") {
-        return handleSymmetricDifference(tokens);
-      }
-
-      return "ERR unknown command \"" + tokens[0] + "\"\n";
-  }
+    std::string handleCommand(std::string_view command);
 
   private:
     Solver solver { };
 
-    std::string handleIntersection(std::vector<std::string> const &tokens) {
-      if (tokens.size() != 1)
-        return "ERR command \"INTERSECTION\" takes no args\n";
-
-      auto intersection { solver.intersection() };
-
-      return formatJoinResult(intersection);
-    }
-
-    std::string handleSymmetricDifference(std::vector<std::string> const &tokens) {
-      if (tokens.size() != 1)
-        return "ERR command \"SYMMETRIC_DIFFERENCE\" takes no args\n";
-
-      auto symDiff { solver.symmetricDifference() };
-
-      return formatJoinResult(symDiff);
-    }
+    std::string handleIntersection(std::vector<std::string> const &tokens);
+    std::string handleSymmetricDifference(std::vector<std::string> const &tokens);
   };
 }
---
+++ src/handler.cpp
@@ -0,0 +1,104 @@
+#include <algorithm>
+#include <boost/algorithm/string.hpp>
+#include <boost/lexical_cast.hpp>
+#include <sstream>
+
+#include "handler.hpp"
+
+using namespace std;
+using namespace otus;
+
+string Handler::handleCommand(string_view command) {
+  vector<string> tokens;
+  boost::split(tokens, command, [](char c) { return c == ' '; });
+
+  transform(
+      tokens[0].begin(),
+      tokens[0].end(),
+      tokens[0].begin(),
+      ::toupper);
+
+  if (tokens[0] == "INSERT") {
+    if (tokens.size() != 4)
+      return "ERR command \"INSERT\" takes exactly three args\n";
+
+    int id;
+    try {
+      id = boost::lexical_cast<int>(tokens[2]);
+    }
+    catch (boost::bad_lexical_cast const &e) {
+      return "ERR invalid id \"" + tokens[2] + "\"\n";
+    }
+
+    auto const &name { tokens[3] };
+
+    if (tokens[1] == "A") {
+      solver.insertToA(id, name);
+      return "OK\n";
+    }
+    else if (tokens[1] == "B") {
+      solver.insertToB(id, name);
+      return "OK\n";
+    }
+    else return "Unknown table \"" + tokens[1] + "\"\n";;
+  }
+  else if (tokens[0] == "TRUNCATE") {
+    if (tokens.size() != 2)
+      return "ERR command \"TRUNCATE\" takes exactly one arg\n";
+    else if (tokens[1] == "A") {
+      solver.truncateA();
+      return "OK\n";
+    }
+    else if (tokens[1] == "B") {
+      solver.truncateB();
+      return "OK\n";
+    }
+    else return "Unknown table \"" + tokens[1] + "\"\n";;
+  }
+  else if (tokens[0] == "INTERSECTION") {
+    return handleIntersection(tokens);
+  }
+  else if (tokens[0] == "SYMMETRIC_DIFFERENCE") {
+    return handleSymmetricDifference(tokens);
+  }
+
+  return "ERR unknown command \"" + tokens[0] + "\"\n";
+}
+
+string Handler::handleIntersection(vector<string> const &tokens) {
+  if (tokens.size() != 1)
+    return "ERR command \"INTERSECTION\" takes no args\n";
+
+  auto intersection { solver.intersection() };
+
+  return formatJoinResult(intersection);
+}
+
+string Handler::handleSymmetricDifference(vector<string> const &tokens) {
+  if (tokens.size() != 1)
+    return "ERR command \"SYMMETRIC_DIFFERENCE\" takes no args\n";
+
+  auto symDiff { solver.symmetricDifference() };
+
+  return formatJoinResult(symDiff);
+}
+
+string otus::formatJoinResult (Solver::JoinType &input) {
+  sort(input.begin(), input.end(), [](auto const &a, auto const &b) {
+      return get<0>(a) < get<0>(b);
+      });
+
+  stringstream result { };
+
+  for (auto line: input) {
+    auto id { to_string(get<0>(line)) };
+    auto const &nameA { get<1>(line) };
+    auto const &nameB { get<2>(line) };
+
+    result << id << ',' << nameA << ',' << nameB << '\n';
+  }
+
+  result << "OK\n";
+
+  return result.str();
+}
