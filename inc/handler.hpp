5e0d9fcde610b249ad42bb406beda37e8bcfc3c9
Anton Karmanov <bergertroll@insiberia.net>
2020-08-02 16:20:04 +0500

    Implement INSERT, TRUNCATE

--- inc/handler.hpp
+++ inc/handler.hpp
@@ -2,13 +2,15 @@
 
 #include <algorithm>
 #include <boost/algorithm/string.hpp>
+#include <boost/lexical_cast.hpp>
+#include <string>
 
 #include "solver.hpp"
 
 namespace otus {
   class Handler {
   public:
-    std::string handleCommand(std::string const &command) {
+    std::string handleCommand(std::string_view command) {
       std::vector<std::string> tokens;
       boost::split(tokens, command, [](char c) { return c == ' '; });
 
@@ -21,12 +23,39 @@ namespace otus {
       if (tokens[0] == "INSERT") {
         if (tokens.size() != 4)
           return "Error: command \"INSERT\" takes exactly three args\n";
-        return "insertion\n";
+
+        int id;
+        try {
+          id = boost::lexical_cast<int>(tokens[2]);
+        }
+        catch (boost::bad_lexical_cast const &e) {
+          return "Error: invalid id \"" + tokens[2] + "\"\n";
+        }
+
+        auto const &name { tokens[3] };
+
+        if (tokens[1] == "A") {
+          solver.insertToA(id, name);
+          return "OK\n";
+        }
+        else if (tokens[1] == "B") {
+          solver.insertToB(id, name);
+          return "OK\n";
+        }
+        else return "Unknown table \"" + tokens[1] + "\"\n";;
       }
       else if (tokens[0] == "TRUNCATE") {
         if (tokens.size() != 2)
           return "Error: command \"TRUNCATE\" takes exactly one arg\n";
-        return "truncating\n";
+        else if (tokens[1] == "A") {
+          solver.truncateA();
+          return "OK\n";
+        }
+        else if (tokens[1] == "B") {
+          solver.truncateB();
+          return "OK\n";
+        }
+        else return "Unknown table \"" + tokens[1] + "\"\n";;
       }
       else if (tokens[0] == "INTERSECTION") {
         if (tokens.size() != 1)
@@ -40,7 +69,7 @@ namespace otus {
       }
 
       return "Error: unknown command \"" + tokens[0] + "\"\n";
-    }
+  }
 
   private:
     Solver solver { };
